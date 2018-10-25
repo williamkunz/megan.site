db02125efaee30df1a528301ad4842e62d58a8d3
FridaDali83 <44450803+FridaDali83@users.noreply.github.com>
2018-10-25 18:00:47 -0400

    updated the image descriptions (#3)
    updated the image descriptions for the print and digital images. I also updated the background color.

--- constants/digital.js
+++ constants/digital.js
@@ -1,14 +1,14 @@
 export default {
   "images": [
     {
-      "name": "my digital 1",
+      "name": "Coatings+ Email Graphic",
       "thumbnail": {
         "alt": "Email to promote the Coatings+ Conference.",
         "1x": "/static/digital/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/digital/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Email to promote the Coatings+ Conference.",
+        "description": "This email graphic was designed to make conference attendees aware that they can start booking their rooms for the upcoming SSPC annual show. This email was sent out to 100,000 plus people worldwide.",
         "images": [
           {
             "alt": "Email to promote the Coatings+ Conference.",
@@ -20,14 +20,14 @@ export default {
       "uniqueId": "coatings-email"
     },
     {
-      "name": "my digital 1",
+      "name": "Envelope Email",
       "thumbnail": {
         "alt": "SSPC 2018 email graphic",
         "1x": "/static/digital/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/digital/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Email to promote the annual SSPC 2018 Conference.",
+        "description": "I was tasked with creating a graphic for an email that would be sent out to over 100,000 people worldwide. We wanted something that would standout and get people's attention. I chose to use bright colors, bold fonts and a unique shape to grab the attention of the viewer.",
         "images": [
           {
             "alt": "SSPC 2018 email graphic",
@@ -39,14 +39,14 @@ export default {
       "uniqueId": "envelope-email"
     },
     {
-      "name": "my digital 1",
+      "name": "Follow Us Email Graphic",
       "thumbnail": {
         "alt": "social media email graphic",
         "1x": "/static/digital/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/digital/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Email graphic designed to aware the public of SSPC's social media presence.",
+        "description": "Everyone is looking to get followers on social media and that is what this image was suppose to convey. When I designed this graphic, I took the words 'follow us' quite literally. I like how simple this graphic is and how it can be used by itself and the viewer would still get the intent. " ,
         "images": [
           {
             "alt": "social media email graphic",
@@ -58,14 +58,14 @@ export default {
       "uniqueId": "follow-us-sm-email"
     },
     {
-      "name": "my digital 1",
+      "name": "Welcome Reception Sign",
       "thumbnail": {
         "alt": "Welcome reception sign",
         "1x": "/static/digital/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/digital/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Digital illustration of New Orleans night life. This reception sign was used digitally and was printed to help promote the SSPC 2018 conference welcome reception.",
+        "description": "<p>I love creating digital illustrations and trying new concepts. When I was asked to design a sign for a big welcome reception party at the SSPC annual conference that encompasses the New Orleans vibe, the first things that came to mind were jazz and Bourban Street. What is more representing of a party in New Orleans than Bourban Street? The answer, nothing.</p><p> So I did some research on vintage art in the New Orleans area and decided to create my own version of an abstract painting of night life on Bourban Street. When I showed this piece to my team, they were thrilled with the outcome because it was so different from anything else I've ever done.</p> <p>This image was used digitally on monitors throughout the venue and was also printed and placed outside of the reception hall.</p>",
         "images": [
           {
             "alt": "Welcome reception sign",
@@ -77,14 +77,14 @@ export default {
       "uniqueId": "welcome-reception-sign"
     },
     {
-      "name": "my digital 1",
+      "name": "Welcome to Tampa Reception Sign",
       "thumbnail": {
         "alt": "Welcome tampa sign",
         "1x": "/static/digital/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/digital/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "This digital illustration was used on the welcome reception sign for the SSPC 2017 conference. It was used digitally to promote the reception.",
+        "description": "<p>Every year at the SSPC annual conference we have a big welcome reception. Of course something like this needs to be promoted. I was asked to create a sign that was attention grabbing and didn't have to do anything with the beach, or Tampa night life. </p> <p>I found this to be challenging but I put my thinking cap on and decided to focus on the Spanish culture in Tampa. </p> <p>While researching the area's Spanish culture, I fell in love with the decorative tiles that seemed to be part of a lot of their structures. So I thought about who created these tiles and what they must have looked like many years ago. When I sat down to draw this graphic, a woman decked out in the beautiful, printed, ancient attire came to mind and that's exactly what I created. </p> <p>This image was used digitally to promote the welcome reception party. </p>",
         "images": [
           {
             "alt": "Welcome tampa sign",
--- constants/print.js
+++ constants/print.js
@@ -1,14 +1,14 @@
 export default {
   "images": [
     {
-      "name": "my print 1",
+      "name": "Awards Program",
       "thumbnail": {
         "alt": "Awards program",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Program for the awards reception at the annual SSPC conference.",
+        "description": "This program was created for the SSPC Award's lunch at their annual conference. It's final size was 6 inches tall by 4 inches wide.",
         "images": [
           {
             "alt": "Awards program",
@@ -25,14 +25,14 @@ export default {
       "uniqueId": "awards-program-2018"
     },
     {
-      "name": "my print 1",
+      "name": "DoD Conference Print Advertisement",
       "thumbnail": {
         "alt": "Advertisement for DoD Conference",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Ad to promote registration for the DoD biannual corrosion conference.",
+        "description": "SSPC teamed up with the U.S. Department of Defense to administer their Corrosion Conference. I created all of the graphics, printed pieces, logo, website and anything else graphical they needed for their conference. This ad was create to help promote their conference. In this ad you'll see the logo I created for them as well.",
         "images": [
           {
             "alt": "Advertisement for DoD Conference",
@@ -45,14 +45,14 @@ export default {
       "uniqueId": "dod-logo-ad"
     },
     {
-      "name": "my print 1",
+      "name": "Hurricane Relief Fund Print Advertisement",
       "thumbnail": {
         "alt": "Advertisement to promote the SSPC hurricane relief fund.",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Advertisement to promote the SSPC hurricane relief fund. The goal with this campaign was to boost membership sales.",
+        "description": "I was asked to come up with a way to boost membership at SSPC and I came up with the idea of giving new members a discount on the cost of membership when they donated to the SSPC hurricane relief fund. It was around this time that hurricane Harvey hit Texas, and I wanted to come up with a way for new members to feel like they we're doing good and also getting something in return (a discounted membership). This proved to work, because within the 3 months this campaign ran, we saw a 25 percent increase in membership. ",
         "images": [
           {
             "alt": "Advertisement to promote the SSPC hurricane relief fund.",
@@ -65,14 +65,14 @@ export default {
       "uniqueId": "hurricane-ad"
     },
     {
-      "name": "my print 1",
+      "name": "Membership Print Advertisement",
       "thumbnail": {
         "alt": "Membership ad",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Advertisement to promote the SSPC membership.",
+        "description": "One way for a non-profit like SSPC to make money is through offering memberships. In the past, they ran ads that had a long list of benefits, and several paragraphs describing how these benefits can help someone if they become a SSPC member. I asked to design a new ad to promote their membership, because no one reads anymore and what they really needed was an ad that was straight to the point. I feel like that is what I accomplished here.",
         "images": [
           {
             "alt": "Membership ad",
@@ -85,14 +85,14 @@ export default {
       "uniqueId": "membership-ad"
     },
     {
-      "name": "my print 1",
+      "name": "SSPC 2018 Onsite Guide",
       "thumbnail": {
         "alt": "Conference guide",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Conference guide for attendees at the SSPC 2018 annual conference.",
+        "description": "Every year SSPC has an annual conference and due to an older demographic, it is required to have an onsite guide for attendees to reference. This is a huge project that I had to design, edit, and collect all of the content that the guide contained. This guide contains 98 pages, stepping pages and enough text to make someone go cross eyed. My goals when designing this guide was to make sure that the text was easy to read, it was well organized and visually appealing. This piece contains some of the driest content I've ever read, and making it seem fun and worth while was quite a challenge. After everything was and done, I feel that I accomplished everything I set out to do. ",
         "images": [
           {
             "alt": "Conference guide",
@@ -114,14 +114,14 @@ export default {
       "uniqueId": "onsite-sspc-2018"
     },
     {
-      "name": "my print 1",
+      "name": "Two-Page Spread",
       "thumbnail": {
         "alt": "QP 2pg ad",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Two-page spread designed to promote the SSPC QP program.",
+        "description": "<p>A competitor of SSPC's was making false claims about its QP Program. This is a certfication program with many different levels of certifications that has been around since the 80s. Its is a highly reguarded program and people are certified in the QP Program worldwide. The point of this timeline ad is to disprove the competitor's false claims and also show how much the program has grown through the years and still continues to grow to this day.</p><p> Pictured in this ad is the Brooklyn Bridge in NY. I chose to use this image because the contractors that worked on this bridge were QP certified. I also chose this image because most people who become certified in the QP Program work on bridges and bridges have become synonymous with the QP Program.",
         "images": [
           {
             "alt": "QP 2pg ad",
@@ -134,14 +134,14 @@ export default {
       "uniqueId": "qp-2pg-ad"
     },
     {
-      "name": "my print 1",
+      "name": "SSPC 2018 Preliminary Guide",
       "thumbnail": {
         "alt": "SSPC preliminary guide",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Preliminary guide for the annual SSPC conference.",
+        "description": "Before the start of the SSPC annual conference, they mail preliminary guides to last year's attendees so they can get a glimpse of what to expect this year. Every year SSPC has sent out a bland, 8.5x11 magazine to their past guests and it usually doesn't get much impact. So when I sat down to design this guide, I decided to make something different. First I decided to change the shape, it's an 8x8 sqaure, and then I chose to make the design more modern by using monochromatic colors, bold font and a light frame to pull the viewers gaze to the center of the cover. I also changed the order of the guide, used bigger font and colored coded each day so that it's more organized. After recieving their preliminary guide in the mail, a lot of people thanked me for making the guide easier to follow and praised me on the modern design.",
         "images": [
           {
             "alt": "SSPC preliminary guide",
@@ -169,14 +169,14 @@ export default {
       "uniqueId": "sspc-prelim"
     },
     {
-      "name": "my print 1",
+      "name": "SSPC 2018 Conference Print Advertisement",
       "thumbnail": {
         "alt": "SSPC 2018 Conference Ad",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Advertisement to promote the upcoming SSPC 2018 conference.",
+        "description": "I created this advertisement to promote the upcoming SSPC 2018 conference. I love how the bold color and font makes it stand out from the rest of the magazine. It's really hard to miss the message!",
         "images": [
           {
             "alt": "SSPC 2018 Conference Ad",
@@ -189,14 +189,14 @@ export default {
       "uniqueId": "sspc2018-ad"
     },
     {
-      "name": "my print 1",
+      "name": "SSPC 2018 Print Advertisement 2",
       "thumbnail": {
         "alt": "SSPC mobile training unit ad",
         "1x": "/static/print/<uniqueId>/thumbnail/@1x.png",
         "2x": "/static/print/<uniqueId>/thumbnail/@2x.png",
       },
       "features": {
-        "description": "Advertisement for the SSPC Mobile Training Unit.",
+        "description": "This ad was created to make the public aware that registration for the SSPC 2018 conference was still open. I used the same layout I used for the preliminary guide and the onsite guide for this ad so that everything was cohesive. ",
         "images": [
           {
             "alt": "SSPC mobile training unit ad",
